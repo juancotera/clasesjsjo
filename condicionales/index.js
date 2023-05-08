cf6c1c1c7ee8059304fcb0ca3163c79f9f8cfe0f
juancotera <juancoteracastro@gmail.com>
2023-05-08 09:25:18 -0500

    condicionales

---
+++ condicionales/index.js
@@ -0,0 +1,31 @@
+//ejercicio 1
+// if (" ") {
+//   alert( 'Hello' );
+// }
+
+// ejercicio 2
+// let respuesta = prompt("¿Cuál es el nombre “oficial” de JavaScript?")
+
+// if (respuesta == "ECMAScript") {
+//   alert("Correcto");
+// } else {
+//   alert("No lo sabes? ¡ECMAScript!")
+// }
+
+// ejercicio 3
+// let numero = prompt("ingresa un número");
+
+// if (numero == "0") {
+//   alert("0")
+// } else if (numero > "0") {
+//   alert("1")
+// } else if (numero < "0") {
+//   alert("-1")
+// }else {
+//   alert("no registro número")
+// }
+
+//ejercicio 4
+let result = (a + b < 4) ? 'Debajo':
+             'encima';
+alert (result);
